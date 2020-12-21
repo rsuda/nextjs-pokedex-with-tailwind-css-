a5cd1deffcf45794cede50625e772081189fcbc7
robinsuda <61332716+robinsuda@users.noreply.github.com>
2020-12-21 14:50:41 -0600

    build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
+import Link from 'next/link';
 export default function Home({ pokemon }) {
     return (
         <Layout title="NextJS Pokedex">
